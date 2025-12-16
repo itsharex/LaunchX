debafd459d83c12c4df25a2b4105fb0f3c2703da
Eric <ericyuanovo@gmail.com>
2025-12-16 16:46:31 +0800

    Initial Commit

---
+++ LanuchXTests/LanuchXTests.swift
@@ -0,0 +1,17 @@
+//
+//  LanuchXTests.swift
+//  LanuchXTests
+//
+//  Created by Eric on 2025/12/16.
+//
+
+import Testing
+@testable import LanuchX
+
+struct LanuchXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ LanuchXUITests/LanuchXUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LanuchXUITestsLaunchTests.swift
+//  LanuchXUITests
+//
+//  Created by Eric on 2025/12/16.
+//
+
+import XCTest
+
+final class LanuchXUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
